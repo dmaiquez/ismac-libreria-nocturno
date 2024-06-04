a0150ad156426e5a4f6f8731c8a5171f23025d25
NOCTURNO <NOCTURNO@DESKTOP-VNL1012>
2024-06-03 21:04:02 -0500

    DMv0006 Implementacion de cliente dao

--- src/com/distribuida/dao/ClienteDAOImpl.java
+++ src/com/distribuida/dao/ClienteDAOImpl.java
@@ -1,6 +1,12 @@
 package com.distribuida.dao;
 
 import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.distribuida.entities.Cliente;
 
@@ -9,13 +15,25 @@ import com.distribuida.entities.Cliente;
 public class ClienteDAOImpl implements ClienteDAO {
 
 	
+	// SELECT * FROM Cliente  -->> SQL
+	// from Cliente           -->> HQL
+	
+	// SELECT cl.* FROM Cliente as cl --> SQL
+	// SELECT cl FROM Cliente cl      --> HQL
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	
 	@Override
+	@Transactional
 	public List<Cliente> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		Session session = sessionFactory.getCurrentSession();		
+		return session.createQuery("from Cliente",Cliente.class).getResultList();
 	}
 
+	
 	@Override
 	public Cliente findOne(int id) {
 		// TODO Auto-generated method stub
